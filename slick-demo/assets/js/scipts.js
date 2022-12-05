a4792b2e24aead8b0eea358ce16cbb272e9b58d7
BeazyBabyy <brorender@gmail.com>
2022-12-05 20:57:26 +0000

    Updates

--- slick-demo/assets/js/scipts.js
+++ slick-demo/assets/js/scipts.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
 
-    $(' .bubba').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true, 
-        slide: 'div',
-        cssEase:'linear',
-        autoplay: true,
-        autoplayspeed: 2000,
-    });
-
+    $('.fade').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 
 });
